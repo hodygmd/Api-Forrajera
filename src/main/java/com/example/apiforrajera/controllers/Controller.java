7dbc84f52d49f2ab163a2033db18ddece94f5e0d
hodygmd <gmdhody@gmail.com>
2024-06-11 01:27:58 -0600

    add a hello world

---
+++ src/main/java/com/example/apiforrajera/controllers/Controller.java
@@ -0,0 +1,17 @@
+package com.example.apiforrajera.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@CrossOrigin
+@RestController
+@RequestMapping("/")
+public class Controller {
+    @GetMapping
+    public ResponseEntity<String> home(){
+        return new ResponseEntity<>("API Forrajera", HttpStatus.OK);
+    }
+}
