63336b2ea7df820130815c171842c236dbb4e996
hodygmd <gmdhody@gmail.com>
2024-06-11 01:21:44 -0600

    add username and password to EmpleadoDto

--- src/main/java/com/example/apiforrajera/dto/EmpleadoDto.java
+++ src/main/java/com/example/apiforrajera/dto/EmpleadoDto.java
@@ -6,6 +6,8 @@ import lombok.Data;
 public class EmpleadoDto {
     private String clave;
     private String nombre;
+    private String username;
+    private String password;
     private Integer id_puesto;
     private Byte status;
 }
